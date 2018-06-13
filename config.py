36c11ec83932c39dc8568a30e8fc2c07b46af42b
sunzhanpe <sunzhanpe@sina.com>
2018-06-13 23:06:08 +0800

    edit

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 Api = dict(key='a178b0a4b3ef4f279c1fe9802f5f63c1', secret='ba60f149bb8948c8b673d169af862946')
 
 # config
-Config = dict(symbol='ftusdt', buy_amount=10.0000, sell_amount=10.0000)
+Config = dict(symbol='btcusdt', buy_amount=10.0000, sell_amount=10.0000, zhisun = -0.00002, zhiying = 0.00002)
