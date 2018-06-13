cc76d50b80a4bbd508036f16e0b0831c71f73ed1
sunzhanpe <sunzhanpe@sina.com>
2018-06-14 01:19:29 +0800

    ha

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # api
-Api = dict(key='a178b0a4b3ef4f279c1fe9802f5f63c1', secret='ba60f149bb8948c8b673d169af862946')
+Api = dict(key='你的key', secret='你的secret')
 
 # config
 Config = dict(symbol='btcusdt', buy_amount=0.0016, sell_amount=0.0016, zhisun=-0.00002, zhiying=0.00002)
