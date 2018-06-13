5e44a09dd020b8f3dc9f0ef1dcc0328dd583ec86
sunzhanpe <sunzhanpe@sina.com>
2018-06-13 23:54:19 +0800

    aaa

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 Api = dict(key='a178b0a4b3ef4f279c1fe9802f5f63c1', secret='ba60f149bb8948c8b673d169af862946')
 
 # config
-Config = dict(symbol='btcusdt', buy_amount=0.0016, sell_amount=0.0016, zhisun = -0.00002, zhiying = 0.00002)
+Config = dict(symbol='btcusdt', buy_amount=0.0016, sell_amount=0.0016, zhisun=-0.00002, zhiying=0.00002)
